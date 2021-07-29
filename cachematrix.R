6aebfc43e6a7598b48b72ea000a1c3b3cd3d88a4
Aakarsh Rai <raiaakarsh9@gmail.com>
2021-07-29 13:49:02 -0500

    This is the function code for the second programming assignment in Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache<-NULL
+  set<-function(y){
+    x<<-y
+    cache<-NULL
+  }
+  get <- function()x
+  setinverse <- function(inverse) cache<<-inverse
+  getinverse <- function() cache
+  
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  cache <- x$getinverse()
+  
+  if(!is.null(cache)){
+    return(cache)
+  }
+  matrix <- x$get()
+  cache <- solve(matrix)
+  x$setinverse(cache)
+  cache
 }
